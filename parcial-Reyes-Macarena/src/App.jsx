38bcb7fe2fdd5c6e48dd5ff70b82428f3c8bc6f3
maca-r <macarenagreyes@gmail.com>
2023-05-29 22:35:13 -0300

    parcial-Reyes-Macarena

---
+++ parcial-Reyes-Macarena/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div className="card">
+        {/* <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button> */}
+        <Card/>
+        <form action="">
+          
+        </form>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
